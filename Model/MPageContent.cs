3cccfa8a2acdb6649380e511c2502a5db66916a3
johnt <johnt@JOHNBIGBROTHER>
2023-05-19 17:49:48 +0300

    Добавьте файлы проекта.

---
+++ Model/MPageContent.cs
@@ -0,0 +1,13 @@
+﻿using CustomWindow.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomWindow.Model
+{
+    internal class MPageContent : Model_MVVM
+    {
+    }
+}
